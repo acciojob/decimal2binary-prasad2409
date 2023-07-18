066d7eaf2a6277aaba21a2738f47609f4b22c1c4
Durga Prasad <97454573+prasad2409@users.noreply.github.com>
2023-07-18 11:36:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function decimalToBinary(num) {
-  //Write you code here
-  
+    let ans = 0;
+    let i = 1;
+    while(num>0)
+    {
+        let rem = num%2;
+        ans = ans + (rem * i);
+        i = i*10;
+        num = parseInt(num/2);
+    }
+	return ans.toString();
 }
 
 window.decimalToBinary = decimalToBinary;
